4cc3b3aa73a3d389ae4184d2a30ffa5b84f7c4be
BristiJana <bristi0654@gmail.com>
2023-11-22 15:36:29 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import Pdf from 'react-native-pdf';
+
+const PDFViewer = () => {
+  const source = {
+    uri: 'http://www.pdf995.com/samples/pdf.pdf', // Replace with your PDF URL
+    cache: true,
+  };
+
+  return (
+    <View style={styles.container}>
+      <Pdf
+        source={source}
+        onLoadComplete={(numberOfPages, filePath) => {
+          console.log(`Number of pages: ${numberOfPages}`);
+        }}
+        onPageChanged={(page, numberOfPages) => {
+          console.log(`Current page: ${page}`);
+        }}
+        onError={error => {
+          console.log('Error while loading PDF:', error);
+        }}
+        style={styles.pdf}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pdf: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
+
+export default PDFViewer;
